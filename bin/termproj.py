371c6ef3f2109e95e053bffe7e80c62cb161ada2
Massimo Vicenzo <mastermass8822@gmail.com>
2019-05-13 01:14:52 -0400

    edits to the plot

--- bin/termproj.py
+++ bin/termproj.py
@@ -2,6 +2,7 @@
 import matplotlib.pyplot as plt
 from PIL import Image
 import math
+import numpy as np
 
 def checkblock(x,y,pixpblo):
     pixpblo = (int)(pixpblo)
@@ -18,12 +19,11 @@ def checkblock(x,y,pixpblo):
                 return 1
     return 0
 
-
-
 ######################################
-im = Image.open("holyshitUK.png")
+
+im = Image.open("UK.png")
 print (im.format, im.size, im.mode)
-im.show()
+#im.show()
 
 imasize = im.size[0]
 pixpblo = (int)((imasize)//2)
@@ -36,7 +36,7 @@ while (pixpblo >= 1):
     y = 0
     for i in range(0,blocknum):
         for n in range(0,blocknum):
-            filled = filled + checkblock(x,y,pixpblo)     #[4, 10, 30, 100, 329, 1177, 4336, 16512]
+            filled = filled + checkblock(x,y,pixpblo)
             x = x + pixpblo
         x = 0
         y = y + pixpblo
@@ -46,15 +46,25 @@ while (pixpblo >= 1):
 print(ans)
 
 ######################################
-
+double = 2
 nvalues = []
 indvalues = []
-for i in range(0,len(ans)-1):
-    n = math.log1p(((ans[i])/(ans[i+1]))-1)/math.log1p(0.5-1)
-    nvalues.append(n)
-    indvalues.append(i+2)
+indvaluesreg = []
+for i in range(0,len(ans)):
+    #n = math.log1p(((ans[i])/(ans[i+1]))-1)/math.log1p(0.5-1)  # we could try n = math.log1p(((ans[i])/(ans[i+1]))-1)/math.log1p(2-1)
+    nvalues.append(math.log1p(ans[i]))
+    indvalues.append(math.log1p(double))
+    double = 2*double
 
 print(nvalues)
 print(indvalues)
-plt.plot(indvalues,nvalues)
+m,b = np.polyfit(indvalues,nvalues,1)
+
+for i in range(0,len(ans)):
+    plt.plot(indvalues[i],nvalues[i],'bo')
+    indvaluesreg.append(m*indvalues[i]+b)
+
+str = (str)(m)
+plt.plot(indvalues, indvaluesreg)
+plt.title(str)
 plt.show()
